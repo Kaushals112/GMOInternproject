58e9c3477bca24beecef49d49a95d8a8ad1f07f1
Kaushals112 <114489640+Kaushals112@users.noreply.github.com>
2023-07-24 08:53:08 +0530

    this is final project

---
+++ src/components/CheckboxForm.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import './Home.css';
+
+interface Department {
+  department: string;
+  sub_departments: string[];
+}
+
+const handlestyle = { padding: 20, height: '60vh', width: 280, margin: '120px auto' };
+
+const res: Department[] = [
+  {
+    "department": "customer_service",
+    "sub_departments": [
+      "support",
+      "customer_success"
+    ]
+  },
+  {
+    "department": "design",
+    "sub_departments": [
+      "graphic_design",
+      "product_design",
+      "web_design"
+    ]
+  }
+];
+
+const DepartmentList: React.FC = () => {
+  const [showSubDepartments, setShowSubDepartments] = useState<{ [key: string]: boolean }>({});
+  const [checked, setChecked] = useState<{ [key: string]: boolean }>({});
+
+  useEffect(() => {
+    // Check if all sub-departments are checked for each department
+    const updatedChecked = { ...checked };
+    res.forEach(item => {
+      const allSubDeptsChecked = item.sub_departments.every(subDept => updatedChecked[subDept]);
+      updatedChecked[item.department] = allSubDeptsChecked;
+    });
+    setChecked(updatedChecked);
+  }, [checked]);
+
+  const handleDepartmentChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newChecked = { ...checked };
+    newChecked[name] = event.target.checked;
+
+    // Check/uncheck all sub-departments based on department checkbox
+    if (event.target.checked) {
+      res.find(item => item.department === name)?.sub_departments.forEach(subDept => {
+        newChecked[subDept] = true;
+      });
+    } else {
+      res.find(item => item.department === name)?.sub_departments.forEach(subDept => {
+        newChecked[subDept] = false;
+      });
+    }
+
+    setChecked(newChecked);
+    setShowSubDepartments(prevShowSubDepartments => ({
+      ...prevShowSubDepartments,
+      [name]: event.target.checked
+    }));
+  };
+
+  const handleSubDepartmentChange = ( subDept: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(prevChecked => ({
+      ...prevChecked,
+      [subDept]: event.target.checked
+    }));
+  };
+
+  return (
+    <div>
+      <Grid>
+        <Paper elevation={10} style={handlestyle}>
+          {res.map((item, index) => (
+            <div key={index} className='Kaushal'>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checked[item.department] || false}
+                    onChange={handleDepartmentChange(item.department)}
+                  />
+                }
+                label={<span style={{ fontWeight: 'bold' }}>{item.department}</span>}
+              />
+              {showSubDepartments[item.department] && (
+                <div>
+                  {item.sub_departments.map((subDept, subIndex) => (
+                    <FormControlLabel
+                      key={subIndex}
+                      control={
+                        <Checkbox
+                          checked={checked[subDept] || false}
+                          onChange={handleSubDepartmentChange( subDept)}
+                        />
+                      }
+                      label={subDept}
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
+
+export default DepartmentList;
